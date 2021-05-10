7237753c48fe31eefe12de8a1c17a3e91eca1560
Suraj Shetty <surajshetty3416@gmail.com>
2021-05-10 14:16:26 +0530

    fix: Update port

--- index.js
+++ index.js
@@ -92,5 +92,5 @@ app.get('/test-status', (req, res) => {
 	})
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}!`))
